c5419a2a5a6a3628c49f33b6bb255383a6c63f7c
david <davidmovsichoff@gmail.com>
2024-11-28 01:31:19 -0300

    unit tests

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,25 +1,63 @@
 // test/index.spec.ts
-import { env, createExecutionContext, waitOnExecutionContext, SELF } from 'cloudflare:test';
+import { env, createExecutionContext, waitOnExecutionContext } from 'cloudflare:test';
 import { describe, it, expect } from 'vitest';
 import worker from '../src/index';
 
-// For now, you'll need to do something like this to get a correctly-typed
 // `Request` to pass to `worker.fetch()`.
 const IncomingRequest = Request<unknown, IncomingRequestCfProperties>;
+const BASE_URL = 'http://127.0.0.1:8787';
 
-describe('Hello World worker', () => {
-	it('responds with Hello World! (unit style)', async () => {
-		const request = new IncomingRequest('http://example.com');
+describe('GET /reviews', () => {
+	it('responds with an array', async () => {
+		const request = new IncomingRequest(BASE_URL + '/api/reviews');
 		// Create an empty context to pass to `worker.fetch()`.
 		const ctx = createExecutionContext();
 		const response = await worker.fetch(request, env, ctx);
 		// Wait for all `Promise`s passed to `ctx.waitUntil()` to settle before running test assertions
 		await waitOnExecutionContext(ctx);
-		expect(await response.text()).toMatchInlineSnapshot(`"Hello World!"`);
-	});
-
-	it('responds with Hello World! (integration style)', async () => {
-		const response = await SELF.fetch('https://example.com');
-		expect(await response.text()).toMatchInlineSnapshot(`"Hello World!"`);
+		expect(await response.json()).toBeInstanceOf(Array);
 	});
 });
+
+describe('POST /reviews', () => {
+	it('responds with the added review data', async () => {
+		const reviewInput = {
+			author: 'Tester',
+			comment: 'Good work man!',
+			rating: 4
+		};
+
+		const request = new IncomingRequest(BASE_URL + '/api/reviews', {
+			method: 'POST',
+			body: JSON.stringify(reviewInput)
+		});
+
+		const ctx = createExecutionContext();
+		const response = await worker.fetch(request, env, ctx);
+
+		await waitOnExecutionContext(ctx);
+		expect(await response.json()).toMatchObject(reviewInput);
+	});
+});
+
+describe('Not defined route', () => {
+	it('responds with status 404', async () => {
+		const request = new IncomingRequest(BASE_URL + '/thisCouldBeAnyUndefinedURL');
+
+		const ctx = createExecutionContext();
+		const response = await worker.fetch(request, env, ctx);
+
+		await waitOnExecutionContext(ctx);
+		expect(await response.status).toBe(404);
+	})
+
+	it('responds with a error object', async () => {
+		const request = new IncomingRequest(BASE_URL + '/thisCouldBeAnyUndefinedURL');
+
+		const ctx = createExecutionContext();
+		const response = await worker.fetch(request, env, ctx);
+
+		await waitOnExecutionContext(ctx);
+		expect(await response.json()).toEqual({status: 404, statusText: 'Not found'});
+	})
+})
